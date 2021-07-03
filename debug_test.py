813cc924293c8c9111e44e4c4a1c9e2e528560e3
array_fish <uekusa_hideaki@yahoo.co.jp>
2021-07-03 19:38:41 +0900

    [add] add pycharm config files [fix] remove redundant import

---
+++ debug_test.py
@@ -0,0 +1,9 @@
+print("hello world")
+
+k=0
+for i in range(7):
+    print(i)
+    k += i
+
+print(k)
+
--- draft_deck/main.py
+++ draft_deck/main.py
@@ -4,13 +4,15 @@ from deck_list_frame import Deck_list_frame
 from card_detail_controller import Card_detail_controller
 from drafter import Drafter
 import tkinter
-from tkinter import ttk
 from parameter import IMG_DIR, DB_FILE, POOL_FILE, DECK_DIR
 
+
 def print_hierarchy(w, depth=0):
-    print('  '*depth + w.winfo_class() + ' w=' + str(w.winfo_width()) + ' h=' + str(w.winfo_height()) + ' x=' + str(w.winfo_x()) + ' y=' + str(w.winfo_y()))
+    print('  ' * depth + w.winfo_class() + ' w=' + str(w.winfo_width()) + ' h=' + str(w.winfo_height()) + ' x=' + str(
+        w.winfo_x()) + ' y=' + str(w.winfo_y()))
     for i in w.winfo_children():
-        print_hierarchy(i, depth+1)
+        print_hierarchy(i, depth + 1)
+
 
 def main():
     root = tkinter.Tk()
@@ -18,18 +20,18 @@ def main():
     root.columnconfigure(0, weight=1)
     root.rowconfigure(0, weight=1)
 
-    drafter = Drafter(IMG_DIR,DB_FILE, POOL_FILE, DECK_DIR)
-    card_detail_frame = Card_detail_frame(root,drafter)
-    card_detail_frame.grid(column=0,row=0)
-    card_detail_controller = Card_detail_controller(drafter,card_detail_frame)
-    card_choice_frame = Card_choice_frame(root,drafter,card_detail_controller)
-    card_choice_frame.grid(column=1,row=0)
-    deck_list_frame = Deck_list_frame(root,drafter,card_detail_controller)
-    deck_list_frame.grid(column=2,row=0)
-    #print_hierarchy(root)
+    drafter = Drafter(IMG_DIR, DB_FILE, POOL_FILE, DECK_DIR)
+    card_detail_frame = Card_detail_frame(root, drafter)
+    card_detail_frame.grid(column=0, row=0)
+    card_detail_controller = Card_detail_controller(drafter, card_detail_frame)
+    card_choice_frame = Card_choice_frame(root, drafter, card_detail_controller)
+    card_choice_frame.grid(column=1, row=0)
+    deck_list_frame = Deck_list_frame(root, drafter, card_detail_controller)
+    deck_list_frame.grid(column=2, row=0)
+    # print_hierarchy(root)
     root.mainloop()
 
 
-
 if __name__ == "__main__":
+    print('Hello')
     main()
